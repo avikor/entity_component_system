eb1547fe6e2219c72d157ccf4516b362c175dc62
Avi <44028161+avikor@users.noreply.github.com>
2018-12-03 17:34:52 +0200

    Create setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ecs",
+    version="0.0.1",
+    author="Avikor",
+    author_email="44028161+avikor@users.noreply.github.com",
+    description="An Entity–Component–System (ECS) framework for writing games with pygame.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avikor/entity_component_system",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
