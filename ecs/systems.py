f57117c8160c4b85d7816e47bab70086334dad22
Avi <44028161+avikor@users.noreply.github.com>
2018-12-05 20:59:55 +0200

    optimized

--- ecs/systems.py
+++ ecs/systems.py
@@ -116,7 +116,10 @@ def lists_collision_detection_system(entities: List[List[Component]],
 
 def lists_collision_detection_with_handling_system(entities: List[List[Component]],
                                                    other_entities: List[List[Component]],
-                                                   entities_manager: EntitiesManager, handler: Callable) -> None:
+                                                   entities_manager: EntitiesManager,
+                                                   handler: Callable[[List[Component], List[List[Component]],
+                                                                      List[int], EntitiesManager], None]) -> None:
+    """  This system receives two lists of entities, and calls """
     other_entities_rects = list()
     for other_entity in other_entities:
         other_entities_rects.append(get_component_of_entity(other_entity, GraphicComponent).rect)
