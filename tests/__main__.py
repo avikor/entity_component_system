9a8a711c89be72f249af9510657bbcc3a9e19e6c
Avi <44028161+avikor@users.noreply.github.com>
2019-01-18 17:41:07 +0200

    optimized

--- tests/__main__.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-if __name__ == '__main__':
-    loader = unittest.TestLoader()
-    suite = loader.discover('tests')
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
